3bd31ea520d419ce05314ec552c15842b9269940
mpanne <merlin.pannewitz@digitalservice.bund.de>
2024-04-29 11:38:51 +0200

    Cleanup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <header>
-      <!--      <nav>-->
-      <!--        <ul>-->
-      <!--          <li><NuxtLink to="/">Home</NuxtLink></li>-->
-      <!--          <li><NuxtLink to="/about">About</NuxtLink></li>-->
-      <!--          <li><NuxtLink to="/requirements">Requirements</NuxtLink></li>-->
-      <!--        </ul>-->
-      <!--      </nav>-->
-    </header>
     <main>
       <slot/>
     </main>
