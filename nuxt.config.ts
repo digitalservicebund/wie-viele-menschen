d1b881b5293f7e0abff9bce70fa603f2be2f1133
mpanne <merlin.pannewitz@digitalservice.bund.de>
2024-05-13 16:36:54 +0200

    Use relative paths for favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,11 +21,11 @@ export default defineNuxtConfig({
         },
       ],
       link: [
-        { rel: "icon", type: "image/png", href: "/favicon-16x16.png" },
+        { rel: "icon", type: "image/png", href: "favicon-16x16.png" },
         {
           rel: "icon",
           type: "image/png",
-          href: "/favicon-32x32.png",
+          href: "favicon-32x32.png",
           sizes: "32x32",
         },
       ],
