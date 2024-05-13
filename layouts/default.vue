385753ed871b05f46292bc83eb7f98ce48aed6fa
mpanne <merlin.pannewitz@digitalservice.bund.de>
2024-05-13 13:59:28 +0200

    Add Links to imprint, data and source code

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,31 @@
     <main>
       <slot />
     </main>
+    <footer>
+      <div class="bg-blue-200 flex justify-center">
+        <div class="p-20 w-full max-w-3xl flex justify-end">
+          <a
+            href="https://digitalservice.bund.de/impressum"
+            class="text-blue-800 underline hover:text-blue-700 p-12"
+            target="_blank">
+            Impressum
+          </a>
+          <a
+              href="https://digitalservice.bund.de/datenschutz"
+              class="text-blue-800 underline hover:text-blue-700 p-12"
+              target="_blank">
+            Datenschutzhinweis
+          </a>
+          <a
+              href="https://github.com/digitalservicebund/a11y-tools"
+              class="text-blue-800 underline hover:text-blue-700 p-12"
+              target="_blank">
+            Quellcode
+          </a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
+<script setup lang="ts">
+</script>
