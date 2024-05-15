33fefeba16a14f74ba261476867ec2783139336d
mpanne <merlin.pannewitz@digitalservice.bund.de>
2024-05-15 13:47:34 +0200

    Add social media meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,29 @@ export default defineNuxtConfig({
           content:
             "Hier können Sie erfahren, wie viele Menschen mit bestimmten Eigenschaften Ihren Service nutzen.",
         },
+        {
+          name: "description",
+          content:
+            "Hier können Sie erfahren, wie viele Menschen mit bestimmten Eigenschaften Ihren Service nutzen.",
+        },
+        { name: "og:title", content: "Wie viele Menschen?" },
+        { name: "og:type", content: "website" },
+        {
+          name: "og:url",
+          content: "https://digitalservicebund.github.io/wie-viele-menschen/",
+        },
+        {
+          name: "og:description",
+          content:
+            "Berechnen Sie, wie viele Menschen eine Behinderung, Beeinträchtigung oder andere Eigenschaften haben, um Maßnahmen abzuleiten, damit Ihr Dienst für alle Menschen funktioniert.",
+        },
+        {
+          name: "og:image",
+          content:
+            "https://digitalservicebund.github.io/wie-viele-menschen/img/preview-social.gif",
+        },
+        { name: "og:image:width", content: "2865" },
+        { name: "og:image:height", content: "1500" },
       ],
       link: [
         { rel: "icon", type: "image/png", href: "favicon-16x16.png" },
