8de5887adf25b3c1d022058a4837baa8987b0085
mpanne <merlin.pannewitz@digitalservice.bund.de>
2024-05-16 16:49:40 +0200

    Use BundesSans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "BundesSansWeb",
+      },
+    },
   },
   presets: [require("@digitalservice4germany/style-dictionary/tailwind")],
   plugins: [require("@digitalservice4germany/angie")],
